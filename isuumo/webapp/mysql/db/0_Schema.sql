8a97a35c7af2c9f45f949702fe6317ae053a053c
Monchi <yukukotani@gmail.com>
2020-09-12 06:15:23 +0000

    index

--- isuumo/webapp/mysql/db/0_Schema.sql
+++ isuumo/webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    INDEX idx_state_search (door_height, door_width, features)
 );
 
 CREATE TABLE isuumo.chair
